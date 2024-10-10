1be5466e56d904db4a0763b2e97bf3ff323de7d8
Ben <Benmiller2001@hotmail.co.uk>
2024-10-10 12:54:43 +0100

    Updates

--- main.js
+++ main.js
@@ -14,50 +14,11 @@ function fetchUsers() {
       users.forEach(user => addUserRow(user));
     });
 }
-function addUserRow(user) {
-  const row = document.createElement('tr');
-  row.innerHTML = `
-    <td>${user.name}</td>
-    <td>${user.email}</td>
-    <td>${user.age}</td>
-    <td>
-      <button onclick="editUser('${user.id}')">Edit</button>
-      <button onclick="deleteUser('${user.id}')">Delete</button>
-    </td>
-  `;
-  userTableBody.appendChild(row);
-}
 
-userForm.addEventListener('submit', function (e) {
-  e.preventDefault();
-  const id = userIdInput.value;
-  const method = id ? 'PUT' : 'POST';
-  const url = id ? `${apiUrl}/${id}` : apiUrl;
 
-  fetch(url, {
-    method,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      name: nameInput.value,
-      email: emailInput.value,
-      age: ageInput.value
-    })
-  }).then(() => {
-    userForm.reset();
-    fetchUsers();
-  });
+userForm.addEventListener('submit', function () {
+
 });
-
-function editUser(id) {
-  fetch(`${apiUrl}/${id}`)
-    .then(res => res.json())
-    .then(user => {
-      userIdInput.value = user.id;
-      nameInput.value = user.name;
-      emailInput.value = user.email;
-      ageInput.value = user.age;
-    });
-}
 
 function deleteUser(id) {
   fetch(`${apiUrl}/${id}`, { method: 'DELETE' }).then(() => {
--- server.js
+++ server.js
@@ -3,36 +3,29 @@ const fs = require('fs');
 const app = express();
 const PORT = 5000;
 
-app.use(express.json()); // Parse incoming JSON requests
+app.use(express.json());
 
-const filePath = './data/users.json'; // File to store users
-
-// Helper to read JSON file
+const filePath = './data/users.json';
 function readUsers() {
   return JSON.parse(fs.readFileSync(filePath, 'utf8'));
 }
 
-// Helper to write JSON file
 function writeUsers(users) {
   fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
 }
 
-// Get all users
 app.get('/api/users', (req, res) => {
   const users = readUsers();
   res.json(users);
 });
 
-// Create a new user
 app.post('/api/users', (req, res) => {
   const users = readUsers();
-  const newUser = { id: Date.now().toString(), ...req.body }; // Simple ID generator
-  users.push(newUser);
+  const newUser = { id: Date.now().toString(), ...req.body };
   writeUsers(users);
   res.status(201).json(newUser);
 });
 
-// Update a user
 app.put('/api/users/:id', (req, res) => {
   const users = readUsers();
   const userIndex = users.findIndex(u => u.id === req.params.id);
@@ -45,16 +38,6 @@ app.put('/api/users/:id', (req, res) => {
     res.status(404).json({ message: 'User not found' });
   }
 });
-
-// Delete a user
-app.delete('/api/users/:id', (req, res) => {
-  let users = readUsers();
-  users = users.filter(u => u.id !== req.params.id);
-  writeUsers(users);
-  res.status(204).end();
-});
-
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
